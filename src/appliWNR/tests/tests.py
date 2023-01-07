1369d895534769f873a0a58a84490c5640ed0ebc
Laurine <laurine.brette@gmail.com>
2023-01-07 16:17:27 +0100

    delet directory models

--- src/appliWNR/tests/tests.py
+++ src/appliWNR/tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from appliWNR.models import Film
 
 # Create your tests here.
 
@@ -50,4 +51,27 @@ class URLTests(TestCase):
     def test_pageDeconnexion(self):
         response = self.client.post('deconnexion/')
         self.assertEqual(response.status_code, 200)
+
+class TestFilm(TestCase):
+
+    def setUp(self):
+        self.film1 =  Film.objects.create(id=1, original_title='Film 1', overview='Film 1 Overview', poster_path='poster1.jpg', production_companies='Company 1')
+        self.film2 = Film.objects.create(id=2, original_title='Film 2', overview='Film 2 Overview', poster_path='poster2.jpg', production_companies='Company 2')
+
+    def test_film_original_title(self):
+        self.assertEqual(self.film1.original_title, 'Film 1')
+        self.assertEqual(self.film2.original_title, 'Film 2')
+    
+    def test_film_overview(self):
+        self.assertEqual(self.film1.overview, 'Film 1 Overview')
+        self.assertEqual(self.film2.overview, 'Film 2 Overview')
+    
+    def test_film_poster_path(self):
+        self.assertEqual(self.film1.poster_path, 'poster1.jpg')
+        self.assertEqual(self.film2.poster_path, 'poster2.jpg')
+    
+    def test_film_production_companies(self):
+        self.assertEqual(self.film1.production_companies, 'Company 1')
+        self.assertEqual(film2.production_companies, 'Company 2')
+
     
